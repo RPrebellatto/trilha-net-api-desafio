378e2560a8e386313905b018e0ede78ab1126854
RPRebellatto <rogerio.rebellatto@gmail.com>
2023-05-09 11:07:06 -0300

    Desafio concluido

--- Models/Tarefa.cs
+++ Models/Tarefa.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,7 @@ namespace TrilhaApiDesafio.Models
 {
     public class Tarefa
     {
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Titulo { get; set; }
         public string Descricao { get; set; }
